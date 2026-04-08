5181b57467175c232f07ef4a421577c3c647cdd6
vishalnaresh51-bit <vishalnaresh51@gmail.com>
2026-04-08 14:53:17 +0530

    Add files via upload

---
+++ labtest.js
@@ -0,0 +1,12 @@
+function calculateSum() {
+    let n = document.getElementById("num").value ;
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0) {
+            sum += i;
+        }
+    }
+
+    document.getElementById("result").innerText = "Sum of even numbers: " + sum;
+}
